c3e63699577037406e42939aa22b694c08866253
tsokac2 <tsokac2@gmail.com>
2020-12-11 12:58:16 +0000

    Adding files cardsspec.js, jasmine-jquery.js, indexjasmine.js to assets folder

---
+++ assets/spec/cardsspec.js
@@ -0,0 +1,43 @@
+describe("Testing DOM Elements - Triggering .on('click') Event in trip.html, work.html and life.html", function(){
+
+    it("Should invoke the jQuery click event when elements #showLinks1 and #hideLinks1 are clicked", function(){
+        setFixtures('<button id="showLinks1" class="load-links-btn">Quick Links...<i class="fas fa-sort-down"></i></button>');
+        $('#showLinks1').on('click', function (){
+            var thisCardLinkShow = "." + this.id + "-grid";
+                $(thisCardLinkShow).show(300);
+                $(".blur").fadeIn(400);
+                $(thisCardLinkShow).addClass("rel-card");
+                $("#Card1").addClass("wrap-rel");
+        });
+
+        setFixtures(' <button id="hideLinks1" class="hideLinks-btn"><i class="far fa-times-circle"></i></button>');
+        $("#hideLinks1").on("click", function (){
+            var thisCardLinkShow = "." + this.id + "-grid";
+                $(thisCardLinkShow).hide(400);
+                $(".blur").fadeOut(300);
+                $("#Card1").removeClass("wrap-rel");
+        });
+
+        var spyEventShow = spyOnEvent('#showLinks1', 'click');
+        var spyEventHide = spyOnEvent('#hideLinks1', 'click');
+        $('#showLinks1').trigger('click');
+        $('#hideLinks1').trigger('click');
+        expect(spyEventShow).toHaveBeenTriggered();
+        expect(spyEventHide).toHaveBeenTriggered();
+    });
+
+
+    
+
+    it("Should invoke the jQuery click event when elements .blur is clicked", function(){
+        setFixtures('<div class="blur"></div>');
+        $(".blur").on("click", function  () {
+            $(".blur").fadeOut(300);
+            $("#Card1").removeClass("wrap-rel");
+            $(".hideLinks1-grid").hide(400);
+         });
+        var spyEventBlur = spyOnEvent('.blur', 'click');
+        $('.blur').trigger('click');
+        expect(spyEventBlur).toHaveBeenTriggered();
+    });
+});
