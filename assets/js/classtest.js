3343cdfdca924a8eac76efd43bbfbba7b63e1e2b
tsokac2 <tsokac2@gmail.com>
2020-12-11 11:38:40 +0000

    Changing ().click event reference in cards.js with .on(click) event trigger

--- assets/js/classtest.js
+++ assets/js/classtest.js
@@ -1,15 +1,1 @@
-function TestClass (className) {
-    var testElement = document.getElementById("mainNav").classList.contains(className);
-
-    if(testElement == true){
-        return true; 
-    } else {
-        return false; 
-    }
-};
-
-TestClass("true");
-
-
-
 
