b16028bede9aef23643b8450f69c24c6ffb990f0
tsokac2 <tsokac2@gmail.com>
2020-12-21 12:32:01 +0100

    Testing .gitignore file for API KEY ignoring proccess

--- assets/libraries/scripts/emailjs.js
+++ assets/libraries/scripts/emailjs.js
@@ -1,3 +1,9 @@
 (function() {
-emailjs.init("user_hI6S08d1aK1XKKU2VWtOI");
+  var username = config.MY_KEY_EMAILJS_USER;
+emailjs.init(username);
 })();
+
+
+// (function() {
+// emailjs.init("user_hI6S08d1aK1XKKU2VWtOI");
+// })();
